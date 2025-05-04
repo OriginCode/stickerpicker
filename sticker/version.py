133280daf106c99e53da18528dfdf386582a81a8
Kaiyang Wu <self@origincode.me>
2025-05-03 19:06:16 -0700

    20250503
    Signed-off-by: Kaiyang Wu <self@origincode.me>

--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '4b96d236'
-version = '0.1.0+dev.4b96d236'
-linkified_version = '0.1.0+dev.[4b96d236](https://github.com/maunium/stickerpicker/commit/4b96d236212b1212976f4c3c60479e7aaed866cb)'
+git_revision = '80b51437'
+version = '0.1.0+dev.80b51437'
+linkified_version = '0.1.0+dev.[80b51437](https://github.com/maunium/stickerpicker/commit/80b5143718e2edf6653a4077c3304f53a7d5f054)'
